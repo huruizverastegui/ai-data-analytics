2a807a00d8e2bcc75388b256cd003f2e87c3646a
huruizverastegui <118810504+huruizverastegui@users.noreply.github.com>
2024-09-18 08:11:36 +0700

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,11 @@
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Talk to your docs",
+    page_icon="👋",
+)
+st.sidebar.success("Select a page above.")
+
+st.header("Talk to your Evaluation docs")
+st.write("Select a page from the sidebar to get started")
